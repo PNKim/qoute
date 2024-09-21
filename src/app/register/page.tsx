5c75c3543b6916000c0316e6b3062ac94706c4cb
PNKim <paljapon.nait@gmail.com>
2024-09-21 17:36:12 +0700

    feat: register page ui

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
 
 type typeInput = string | number;
 
-export default function Login() {
+export default function Register() {
   const [username, setUsername] = useState<typeInput>("");
   const [password, setPassword] = useState<typeInput>("");
-  const router = useRouter();
 
   const usernameChange = (e: { target: { value: typeInput } }) => {
     setUsername(e.target.value);
@@ -20,7 +18,7 @@ export default function Login() {
   return (
     <section className="w-full h-screen flex justify-center items-center">
       <div className="bg-slate-300 p-10 min-w-[300px] w-[25%] rounded-2xl flex flex-col justify-center items-center gap-6">
-        <header className="text-center font-bold ">Login</header>
+        <header className="text-center font-bold ">Register</header>
         <main>
           <form className="flex flex-col gap-2">
             <label htmlFor="Username" className="font-semibold">
@@ -47,15 +45,6 @@ export default function Login() {
         <footer className="w-full flex justify-center gap-2">
           <button
             type="button"
-            className="text-white font-semibold bg-orange-500 hover:bg-orange-700 py-2 px-6 rounded-full"
-          >
-            login
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              router.push("/register");
-            }}
             className="text-balck font-semibold bg-white hover:text-white hover:bg-slate-400  py-2 px-6 rounded-full"
           >
             Register
